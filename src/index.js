c9e0d65c6e9e60ce02aef9045edd52320807754a
Elcoosp <elcoosp@gmail.com>
2018-05-03 22:49:22 +0200

    add: formalize function
    take fields definition array and return a component with a pickFields prop which is a function returning an order array of fields to render (they are picked from the fieldsNameObject derived from fieldsDefinitions)

--- src/index.js
+++ src/index.js
@@ -1,33 +1,53 @@
 import React, { Component, Fragment } from 'react'
 import ReactDOM from 'react-dom'
 import Formed from './Formed'
+
+const pipe = (initAcc, ...fns) => fns.reduce((acc, f) => f(acc), initAcc)
+const toNameKeyObj = arrayOfObjWithNameKey =>
+  arrayOfObjWithNameKey.reduce((acc, obj) => {
+    acc[obj.name] = obj.name
+    return acc
+  }, {})
+
+const mapFieldsNameToDefs = fieldsDefs => fieldsNameArr =>
+  fieldsNameArr.map(name => fieldsDefs.find(fieldObj => fieldObj.name === name))
+
+const formalize = fieldsDefinitions => ({ pickFields, ...props }) => (
+  <Formed
+    {...props}
+    fields={pipe(
+      fieldsDefinitions,
+      toNameKeyObj,
+      pickFields,
+      mapFieldsNameToDefs(fieldsDefinitions)
+    )}
+  />
+)
+
+const Formalized = formalize([
+  {
+    name: 'pseudo',
+    validators: [
+      [({ pseudo }) => pseudo.length >= 3, 'Pseudo need to be more than 3'],
+      [({ pseudo }) => pseudo.length <= 5, 'Pseudo need to be less than 5']
+    ]
+  },
+  {
+    name: 'password',
+    validators: [[({ password }) => password.length >= 3, 'Password need to be more than 3']]
+  }
+])
+
 class App extends Component {
   onSubmit = (values, injectErrors) =>
     setTimeout(() => {
-      injectErrors({ password: 'wrong' })
+      injectErrors({ pseudo: 'wrong' })
     }, 2000)
 
   render() {
     return (
       <div>
-        <Formed
-          fields={[
-            {
-              name: 'pseudo',
-              validators: [
-                [({ pseudo }) => pseudo.length >= 3, 'Pseudo need to be more than 3'],
-                [({ pseudo }) => pseudo.length <= 5, 'Pseudo need to be  less 5']
-              ]
-            },
-            {
-              name: 'password',
-              validators: [
-                [({ password }) => password.length >= 3, 'Password need to be more than 3']
-              ]
-            }
-          ]}
-          submit={this.onSubmit}
-        />
+        <Formalized pickFields={({ pseudo }) => [pseudo]} submit={this.onSubmit} />
       </div>
     )
   }
